a27591b1fe4ba6a09ccc7f49b982b05ba60ac668
Alexandria Burnett <thekaylisia@gmail.com>
2022-03-30 14:51:07 -0500

    Added first component

--- src/main.js
+++ src/main.js
@@ -6,4 +6,6 @@ const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount('#app') /*mount the app and tell it the element we want to find and 
+control for our vue app
+Every vue application starts by creating a new vue instance and mounting it */
