4112226abbc3100b24b509d615eb1a5e3c058871
Alexandria Burnett <thekaylisia@gmail.com>
2022-04-02 13:55:01 -0500

    Added search to news list component

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,4 +1,13 @@
 <template>
+<form  @submit.prevent="searchNews" class="d-flex flex-column justify-content-center" >
+   <div class="input-group mx-sm-3 mb-2">
+   <label class="visually-hidden" for="search">Search</label>
+   <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
+search term here" />
+  <button class="btn btn-primary mb-2">Search</button>
+  </div>
+ <p>You are searching for {{ searchTerm }}</p>
+ </form>
       <div class="news__list">
            <div v-for="article in articles" class="news__item">
               <img v-bind:src= article.urlToImage>
@@ -11,7 +20,23 @@
 <script>
 export default {
     data() {
-      return { articles:[] }     
+      return { articles:[],
+              searchTerm:''}     
+    },
+    methods:{
+      searchNews(){
+        let self = this;
+        fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en',{
+          headers:{'Authorization':`Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`}
+        })
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(data){
+          console.log(data);
+          self.articles=data.articles;
+        });
+      }
     },
     created(){
       let self=this;
@@ -44,7 +69,7 @@ export default {
     min-width: min-content;
     background-color: white;
     border:2px;
-    border-radius: 10px;
+    border-radius: 5px;
     border-bottom: 5px solid #3ebb43;
     /*box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;*/
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
---
+++ src/views/NewsView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="news container">
+    <h2>News</h2>
+    </div>
+</template>
