9389c59ed6390b22f061083277c460d1c6c78fee
Alexandria Burnett <thekaylisia@gmail.com>
2022-04-02 14:10:17 -0500

    Added news route

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -2,7 +2,7 @@
 <form  @submit.prevent="searchNews" class="d-flex flex-column justify-content-center" >
    <div class="input-group mx-sm-3 mb-2">
    <label class="visually-hidden" for="search">Search</label>
-   <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
+   <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter 
 search term here" />
   <button class="btn btn-primary mb-2">Search</button>
   </div>
@@ -83,7 +83,6 @@ img{
     width: 100%;
     max-height:220px;
 }
-
 h2{
 font-size:25px;
 }
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,5 +1,27 @@
 <template>
-    <div class="news container">
-    <h2>News</h2>
+    <div class="news_container">
+    <h1> News </h1>
+     <NewsList/>
     </div>
+   
 </template>
+
+<script>
+import NewsList from '@/components/NewsList.vue'
+
+export default{
+    data(){
+        return{};
+    },
+      components:{ NewsList }
+};
+</script>
+
+<style>
+.news_container{
+    margin:20px;
+}
+h1{
+text-align:center;
+}
+</style>
